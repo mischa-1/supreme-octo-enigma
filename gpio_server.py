6d702dcad79fb1a80f7c2477813d9999838cbc2a
Mischa Trainor <112340259+mischa-1@users.noreply.github.com>
2026-03-05 05:21:26 -0600

    Update gpio_server.py

--- gpio_server.py
+++ gpio_server.py
@@ -5,11 +5,21 @@ import time
 HOST = "0.0.0.0"
 PORT = 5000
 
-GPIO_PIN = 21
+LEFT_PIN = 21    
+CENTER_PIN = 18
+RIGHT_PIN = 27
 
 # open gpio
 h = lgpio.gpiochip_open(0)
-lgpio.gpio_claim_output(h, GPIO_PIN)
+
+lgpio.gpio_claim_output(h, LEFT_PIN)
+lgpio.gpio_claim_output(h, CENTER_PIN)
+lgpio.gpio_claim_output(h, RIGHT_PIN)
+
+def pulse(pin):
+    lgpio.gpio_write(h, pin, 1)
+    time.sleep(0.2)
+    lgpio.gpio_write(h, pin, 0)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
@@ -23,15 +33,13 @@ while True:
 
     print("Received:", data)
 
-    if data == "ON":
-        lgpio.gpio_write(h, GPIO_PIN, 1)
+    if cmd == "LEFT":
+        pulse(LEFT_PIN)
 
-    elif data == "OFF":
-        lgpio.gpio_write(h, GPIO_PIN, 0)
+    elif cmd == "CENTER":
+        pulse(CENTER_PIN)
 
-    elif data == "PULSE":
-        lgpio.gpio_write(h, GPIO_PIN, 1)
-        time.sleep(0.2)
-        lgpio.gpio_write(h, GPIO_PIN, 0)
+    elif cmd == "RIGHT":
+        pulse(RIGHT_PIN)
 
     conn.close()
